f303c51e308ae253e5cfcb146e53a463b49d6e89
Alan <tiltedveggie@outlook.kr>
2022-11-12 07:01:07 -0300

    Update

--- src/Database/worksDB.js
+++ src/Database/worksDB.js
@@ -1,4 +1,21 @@
 const works = [
+
+	{
+		img: "./media/projects-thumbnails/CofCof-Studios.jpg",
+		name: "CofCof Studios",
+		tools: "SASS, React.js, Next.js, Axios, Firebase, API REST, Swiper.js, Git, Figma",
+		description: "I am currently developing the Frontend part of a portfolio for CofCof Studios, a 3D design and rendering company from Buenos Aires, Argentina.",
+		link: "https://www.figma.com/proto/6VNW5wyMLdr6qwBadYcnu7/CofCof-Studios?page-id=0%3A1&node-id=86%3A17&viewport=2787%2C-1538%2C0.85&scaling=scale-down&starting-point-node-id=86%3A17"
+	},
+
+	{
+		img: "./media/projects-thumbnails/inner-team.jpg",
+		name: "inner-team",
+		tools: "Tailwind, React.js, Git, GitHub, Vercel, Figma",
+		description: 'I was one of the people in charge of the Frontend part of the project for the company, I built all the content starting from the "FAQ Section" and going down. Also all the "Blog".',
+		link: "https://inner-team-pi.vercel.app/"
+	},
+
 	{
 		img: "./media/projects-thumbnails/Pablo-Emmanuel-De-Leo.jpg",
 		name: "Pablo",
@@ -10,10 +27,12 @@ const works = [
 	{
 		img: "./media/projects-thumbnails/Eliana-Moura.jpg",
 		name: "Eliana Moura",
-		tools: "HTML, SASS, JavaScript, Figma, Photoshop",
+		tools: "HTML, SASS, JavaScript, Git, Figma, Adobe CC",
 		description: "In this project I was responsible for developing a personal page for the Registered Nurse Practitioner Eliana Moura, a recognized worker from our city.",
 		link: "https://tiltedveggie.github.io/Eliana-Moura/"
 	}
+
 ]
+
 
 export default works;
