71cbe2dbb414e9d1c14dd86f77055e8d9e3241fb
Alan <tiltedveggie@outlook.kr>
2023-01-02 14:41:03 -0300

    Update

--- src/Database/skillsDB.js
+++ src/Database/skillsDB.js
@@ -5,100 +5,110 @@ const skills = [
 	},
 
 	{
-		img: "./media/skills-icons/css3.svg",
+		img: "./media/skills-icons/css.svg",
 		name: 'CSS'
 	},
-	
+
 	{
 		img: "./media/skills-icons/javascript.svg",
 		name: 'JavaScript'
 	},
-	
+
 	{
 		img: "./media/skills-icons/emmet.svg",
 		name: 'Emmet'
 	},
-	
+
 	{
 		img: "./media/skills-icons/bootstrap.svg",
 		name: 'Bootstrap'
 	},
-	
+
 	{
 		img: "./media/skills-icons/tailwind.svg",
 		name: 'Tailwind'
 	},
-	
+
 	{
 		img: "./media/skills-icons/sass.svg",
 		name: 'Sass'
 	},
-	
+
 	{
 		img: "./media/skills-icons/jquery.svg",
 		name: 'jQuery'
 	},
-	
+
 	{
 		img: "./media/skills-icons/react.svg",
 		name: 'React.js'
 	},
-	
+
 	{
 		img: "./media/skills-icons/next.svg",
 		name: 'Next.js'
 	},
-	
+
 	{
 		img: "./media/skills-icons/firebase.svg",
 		name: 'Firebase'
 	},
-	
+
 	{
 		img: "./media/skills-icons/git.svg",
 		name: 'Git'
 	},
-	
+
+	{
+		img: "./media/skills-icons/jest.svg",
+		name: 'Cypress'
+	},
+
+	{
+		img: "./media/skills-icons/rtl.svg",
+		name: 'React Testing Library'
+	},
+
 	{
 		img: "./media/skills-icons/cypress.svg",
 		name: 'Cypress'
 	},
-	
+
 	{
 		img: "./media/skills-icons/github.svg",
 		name: 'GitHub'
 	},
-	
+
 	{
 		img: "./media/skills-icons/vercel.svg",
 		name: 'Vercel'
 	},
-	
+
 	{
 		img: "./media/skills-icons/scrum.svg",
 		name: 'Scrum'
 	},
-	
+
 	{
 		img: "./media/skills-icons/figma.svg",
 		name: 'Figma'
 	},
-	
+
 	{
 		img: "./media/skills-icons/spline.svg",
 		name: 'Spline'
 	},
-	
+
 	{
 		img: "./media/skills-icons/photoshop.svg",
 		name: 'Adobe Photoshop'
 	},
-	
+
 	{
 		img: "./media/skills-icons/illustrator.svg",
 		name: 'Adobe Illustrator'
 	},
-	
+
 	{
 		img: "./media/skills-icons/lightroom.svg",
 		name: 'Adobe Lightroom'
