105b8f88d5674ea80fd0af3d5b52af7c78ea2f50
Alan <tiltedveggie@outlook.kr>
2023-04-11 23:21:21 -0300

    Change works images path

--- src/Database/worksDB.js
+++ src/Database/worksDB.js
@@ -1,38 +1,40 @@
 const works = [
-
 	{
-		img: "./media/projects-thumbnails/CofCof-Studios.jpg",
-		name: "CofCof Studios",
-		tools: "SASS, React.js, Next.js, Axios, Firebase, API REST, Swiper.js, Git, Figma",
-		description: "I worked developing the Frontend part of a portfolio for CofCof Studios, a 3D design and rendering company from Buenos Aires, Argentina.",
-		link: "https://cofcofstudios.com/"
+		thumbnail: 'CofCof-Studios.jpg',
+		name: 'CofCof Studios',
+		tools:
+			'SASS, React.js, Next.js, Axios, Firebase, API REST, Swiper.js, Git, Figma',
+		description:
+			'I worked developing the Frontend part of a portfolio for CofCof Studios, a 3D design and rendering company from Buenos Aires, Argentina.',
+		link: 'https://cofcofstudios.com/'
 	},
 
 	{
-		img: "./media/projects-thumbnails/inner-team.jpg",
-		name: "inner-team",
-		tools: "Tailwind, React.js, Git, GitHub, Vercel, Figma",
-		description: 'I was one of the people in charge of the Frontend part of the project for the company, I built all the content starting from the "FAQ Section" and going down. Also all the "Blog".',
-		link: "https://inner-team-pi.vercel.app/"
+		thumbnail: 'inner-team.jpg',
+		name: 'inner-team',
+		tools: 'Tailwind, React.js, Git, GitHub, Vercel, Figma',
+		description:
+			'I was one of the people in charge of the Frontend part of the project for the company, I built all the content starting from the "FAQ Section" and going down. Also all the "Blog".',
+		link: 'https://inner-team-pi.vercel.app/'
 	},
 
 	{
-		img: "./media/projects-thumbnails/Pablo-Emmanuel-De-Leo.jpg",
-		name: "Pablo",
-		tools: "HTML, SASS, JavaScript, Ruby, Git, Figma, Adobe CC",
-		description: "I worked as a Front End Developer with some teammates to make a personal portfolio for Pablo Emmanuel De Leo, CEO of CofCof Studios and an important artist in our country.",
-		link: "https://www.pabloemmanueldeleo.com/"
+		thumbnail: 'Pablo-Emmanuel-De-Leo.jpg',
+		name: 'Pablo',
+		tools: 'HTML, SASS, JavaScript, Ruby, Git, Figma, Adobe CC',
+		description:
+			'I worked as a Front End Developer with some teammates to make a personal portfolio for Pablo Emmanuel De Leo, CEO of CofCof Studios and an important artist in our country.',
+		link: 'https://www.pabloemmanueldeleo.com/'
 	},
 
 	{
-		img: "./media/projects-thumbnails/Eliana-Moura.jpg",
-		name: "Eliana Moura",
-		tools: "HTML, SASS, JavaScript, Git, Figma, Adobe CC",
-		description: "In this project I was responsible for developing a personal page for the Registered Nurse Practitioner Eliana Moura, a recognized worker from our city.",
-		link: "https://tiltedveggie.github.io/Eliana-Moura/"
+		thumbnail: 'Eliana-Moura.jpg',
+		name: 'Eliana Moura',
+		tools: 'HTML, SASS, JavaScript, Git, Figma, Adobe CC',
+		description:
+			'In this project I was responsible for developing a personal page for the Registered Nurse Practitioner Eliana Moura, a recognized worker from our city.',
+		link: 'https://tiltedveggie.github.io/Eliana-Moura/'
 	}
-
-]
-
+];
 
 export default works;
