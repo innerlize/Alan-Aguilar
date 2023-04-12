5e7178dc03a73a9c2cf33f0a425bb2eaf151d22b
Alan <tiltedveggie@outlook.kr>
2023-04-11 23:46:41 -0300

    Fix Footer Section having unnecesary imported components and missing React scope for JSX.

--- src/components/Footer-Section/footer.jsx
+++ src/components/Footer-Section/footer.jsx
@@ -1,37 +1,36 @@
-import "./footer.css";
-import { BsMouse } from "react-icons/bs";
-import { BsInstagram } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
-import { TiSocialDribbble } from "react-icons/ti";
-import { TiSocialYoutube } from "react-icons/ti";
-import { TiSocialGithub } from "react-icons/ti";
+import React from 'react';
+import './footer.css';
+import { BsMouse } from 'react-icons/bs';
+import { BsInstagram } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+import { TiSocialGithub } from 'react-icons/ti';
 
 function Footer() {
-  return (
-    <div id="footer" className="container footer-container">
-      <h3>
-        That's all{" "}
-        <a href="#home" className="backTop">
-          <h4>
-            <BsMouse /> - scroll up -
-          </h4>
-        </a>
-      </h3>
-      <div className="social-links">
-        <a href="https://www.instagram.com/tiltedveggie/">
-          <BsInstagram className="social" />
-        </a>
+	return (
+		<div id='footer' className='container footer-container'>
+			<h3>
+				That&apos;s all{' '}
+				<a href='#home' className='backTop'>
+					<h4>
+						<BsMouse /> - scroll up -
+					</h4>
+				</a>
+			</h3>
+			<div className='social-links'>
+				<a href='https://www.instagram.com/tiltedveggie/'>
+					<BsInstagram className='social' />
+				</a>
 
-        <a href="#">
-          {" "}
-          <FaFacebookF className="social" />
-        </a>
+				<a href='#'>
+					{' '}
+					<FaFacebookF className='social' />
+				</a>
 
-        <a href="https://github.com/tiltedveggie">
-          <TiSocialGithub className="social" />
-        </a>
-      </div>
-    </div>
-  );
+				<a href='https://github.com/tiltedveggie'>
+					<TiSocialGithub className='social' />
+				</a>
+			</div>
+		</div>
+	);
 }
 export default Footer;
